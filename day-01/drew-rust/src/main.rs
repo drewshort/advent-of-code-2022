20f3375ba8bdc433f593033c143faee022414bf3
Drew Short <github@drew.sothr.com>
2022-12-10 12:33:29 -0600

    Applied rustfmt

--- day-01/drew-rust/src/main.rs
+++ day-01/drew-rust/src/main.rs
@@ -1,11 +1,18 @@
-use std::{env, error::Error, fs::File, fmt, io::{self, BufRead, BufReader, Lines}, path::Path};
+use std::{
+    env,
+    error::Error,
+    fmt,
+    fs::File,
+    io::{self, BufRead, BufReader, Lines},
+    path::Path,
+};
 
 // Override the alias to use `Box<error::Error>`.
 type Result<T> = std::result::Result<T, Box<dyn Error>>;
 
 #[derive(Debug, Clone)]
 struct RuntimeError {
-    message: String
+    message: String,
 }
 
 impl fmt::Display for RuntimeError {
@@ -19,22 +26,26 @@ impl Error for RuntimeError {}
 #[derive(Debug, Clone, Copy)]
 struct Elf {
     id: u8,
-    calories: u32
+    calories: u32,
 }
 
 fn read_lines<P>(file_path: P) -> io::Result<Lines<BufReader<File>>>
-where P: AsRef<Path>, {
+where
+    P: AsRef<Path>,
+{
     let file = File::open(file_path)?;
     Ok(BufReader::new(file).lines())
 }
 
 fn parse_elves(input_file_path: &str) -> Result<Vec<Elf>> {
     let input_file = Path::new(input_file_path);
-    if ! input_file.exists() {
+    if !input_file.exists() {
         let error_message = format!("Path {} does not appear to exist", input_file_path);
-        return Err(Box::new(RuntimeError{message: error_message}));
+        return Err(Box::new(RuntimeError {
+            message: error_message,
+        }));
     }
-    
+
     let mut elves: Vec<Elf> = Vec::new();
     if let Ok(lines) = read_lines(input_file) {
         let mut current_elf_id: u8 = 1;
@@ -46,14 +57,17 @@ fn parse_elves(input_file_path: &str) -> Result<Vec<Elf>> {
                     let trimmed_line = line.trim();
                     // Record the totals for an elf when encountering a newline
                     if trimmed_line.len() < 1 {
-                        elves.push(Elf{id: current_elf_id, calories: current_elf_calories});
+                        elves.push(Elf {
+                            id: current_elf_id,
+                            calories: current_elf_calories,
+                        });
                         current_elf_id += 1;
                         current_elf_calories = 0;
                         continue;
                     }
                     let line_calories: u32 = trimmed_line.parse()?;
                     current_elf_calories += line_calories;
-                },
+                }
                 Err(err) => {
                     return Err(Box::new(err));
                 }
@@ -62,17 +76,22 @@ fn parse_elves(input_file_path: &str) -> Result<Vec<Elf>> {
 
         // Capture the last elf in the event that the file does not end in a newline
         if current_elf_calories != 0 {
-            elves.push(Elf{id: current_elf_id, calories: current_elf_calories});
+            elves.push(Elf {
+                id: current_elf_id,
+                calories: current_elf_calories,
+            });
         }
     }
-    
+
     Ok(elves)
 }
 
 fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
-        return Err(Box::new(RuntimeError{message: String::from("Must provide input file path")}));
+        return Err(Box::new(RuntimeError {
+            message: String::from("Must provide input file path"),
+        }));
     }
     let input_path = &args[1];
     let mut elves = parse_elves(input_path)?;
@@ -81,8 +100,14 @@ fn main() -> Result<()> {
 
     let top_3_elves = &elves[0..3];
     // println!("{:?}", top_3_elves);
-    println!("{} Calories carried by elf {}", top_3_elves[0].calories, top_3_elves[0].id);
-    println!("{} Calories carried by the top 3 elves", top_3_elves.iter().map(|&elf| elf.calories).sum::<u32>());
+    println!(
+        "{} Calories carried by elf {}",
+        top_3_elves[0].calories, top_3_elves[0].id
+    );
+    println!(
+        "{} Calories carried by the top 3 elves",
+        top_3_elves.iter().map(|&elf| elf.calories).sum::<u32>()
+    );
 
     Ok(())
 }
